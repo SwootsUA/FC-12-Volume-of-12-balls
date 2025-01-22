4d29b8d8e05ead71c777de5d57e1640c699d75bf
Kostiantyn <kostya20022210@gmail.com>
2025-01-23 00:40:31 +0200

    Refactor calculation to use integer division

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,8 @@ const smallestBallOuterDiameter = 0.5;
 let totalVolume = 0;
 
 for (let i = 0; i < numberOfBalls; i++) {
-    let innerRadius = (smallestBallOuterDiameter / 2.0) + thicknessOfWalls * (i - 1);
-    let sphereVolume = 4.0 / 3.0 * Math.PI * innerRadius;
+    let innerRadius = (smallestBallOuterDiameter / 2) + thicknessOfWalls * (i - 1);
+    let sphereVolume = 4 / 3 * Math.PI * innerRadius;
     totalVolume += sphereVolume;
 }
 
