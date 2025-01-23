6b1b2b61581ae757969d6d26ab356663ddd2d947
Kostiantyn <kostya20022210@gmail.com>
2025-01-23 12:48:05 +0200

    Rename const to snake_case

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const numberOfBalls = 12;
-const thicknessOfWalls = 0.01;
-const smallestBallOuterDiameter = 0.5;
+const BALLS_COUNT = 12;
+const WALL_THICKNESS = 0.01;
+const SMALLEST_BALL_OUTER_DIAMETER = 0.5;
 
 let totalVolume = 0;
 
-for (let i = 0; i < numberOfBalls; i++) {
-    let innerRadius = (smallestBallOuterDiameter / 2) + thicknessOfWalls * (i - 1);
+for (let i = 0; i < BALLS_COUNT; i++) {
+    let innerRadius = (SMALLEST_BALL_OUTER_DIAMETER / 2) + WALL_THICKNESS * (i - 1);
     let sphereVolume = 4 / 3 * Math.PI * Math.pow(innerRadius, 3);
     totalVolume += sphereVolume;
 }
